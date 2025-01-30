cf7bf161b62535713500d5d661a744e8d2d178ed
Matías Martínez <soy@matmartinez.net>
2025-01-30 20:14:35 -0300

    Added alias for iA Writer.

--- aliases.sh
+++ aliases.sh
@@ -6,3 +6,6 @@ alias tower="/Applications/Tower.app/Contents/MacOS/gittower"
 
 # Nova CLI.
 alias nova="/Applications/Nova.app/Contents/SharedSupport/nova"
+
+# iA Writer.
+alias ia='open -a "iA Writer"'
